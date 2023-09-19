8e22f3e4eed59397d41e797cdc359883ece4b399
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-09-19 18:47:31 +0700

    Add default page for signup

---
+++ chatmeleon-app/app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+    return (
+      <div>
+        <p className='text-sky-500 text-3xl'>
+          THIS IS THE SIGN UP PAGE
+        </p>
+      </div>
+    )
+  }
+  
