63bc3f0ffaebd955addfed1406dbda131913ea9a
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-09-19 18:47:19 +0700

    Add default page for login

---
+++ chatmeleon-app/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+      <div>
+        <p className='text-sky-500 text-3xl'>
+          THIS IS THE LOGIN PAGE
+        </p>
+      </div>
+    )
+  }
+  
