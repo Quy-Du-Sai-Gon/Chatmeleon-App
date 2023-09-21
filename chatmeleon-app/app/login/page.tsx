d6e4b03ea256b8f57bdea1d0318a6bca2f64282d
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-09-21 17:39:11 +0700

    Update login/signup UI

--- chatmeleon-app/app/login/page.tsx
+++ chatmeleon-app/app/login/page.tsx
@@ -1,9 +1,42 @@
+import Image from "next/image"
+import AuthForm from "./components/AuthForm"
+
 export default function Login() {
     return (
-      <div>
-        <p className='text-sky-500 text-3xl'>
-          THIS IS THE LOGIN PAGE
-        </p>
+      <div
+        className="
+          flex
+          min-h-full
+          flex-col
+          justify-center
+          py-12
+          sm:px-6
+          lg:px-8
+         bg-white
+        "
+      >
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <Image
+            alt="Black White Logo"
+            height="90"
+            width="90"
+            className="mx-auto w-auto"
+            src="/images/logo-black&white-1.png"
+          />
+          <h2
+            className="
+              mt-6
+              text-center
+              text-2xl
+              font-bold
+              tracking-tight
+              text-gray-900
+              "
+          >
+            Sign in to your Chameleon account
+          </h2>
+        </div>
+        <AuthForm/>
       </div>
     )
   }
