4fa11dddb560a473b706286383a42b5ff9548b90
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-09-24 18:01:16 +0700

    Create AuthContext and Integrate for app

--- chatmeleon-app/app/layout.tsx
+++ chatmeleon-app/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import ToasterContext from './context/ToasterContext';
+import AuthContext from './context/AuthContext';
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={poppins.className}>
       <body>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   )
