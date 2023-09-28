dae1e746f320599d6ce36a4fcd15e925d50fad1b
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-09-28 16:01:28 +0700

    Create middleware

---
+++ chatmeleon-app/app/middleware.ts
@@ -0,0 +1,15 @@
+// Import dependencies
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+    pages: {
+        signIn: "/auth"
+    }
+})
+
+export const config = {
+    matcher: [
+        "conversations/:path*"
+    ]
+};
+
