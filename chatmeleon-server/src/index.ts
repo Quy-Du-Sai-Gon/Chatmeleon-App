73ba2a9bf18b3f50e07e9c9c85a5e73e43f7219c
unknown <thanhbo025@gmail.com>
2023-10-10 11:23:21 +0700

    CHAT-52: Update PORT for backend

--- chatmeleon-server/src/index.ts
+++ chatmeleon-server/src/index.ts
@@ -1,9 +1,12 @@
 // src/app.ts
 import express from "express";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); // This will read the .env file and set the environment variables
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World! This is your TypeScript Express server.");
