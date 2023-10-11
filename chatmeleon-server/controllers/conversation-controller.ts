5e4ea890c07a2456fda2934185e2ac6b21ed45ec
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-11 23:02:57 +0700

    CHAT-63: Updated conversation-controller.ts and conversation-route.ts. GET /conversations/:id: Retrieve conversation by ID

--- chatmeleon-server/controllers/conversation-controller.ts
+++ chatmeleon-server/controllers/conversation-controller.ts
@@ -2,7 +2,7 @@ import prisma from "../libs/prismadb.js"
 import { Request, Response } from "express";
 
 // Define your async route handler function
-const getAllConverstations = async (req: Request, res: Response) => {
+const getAllConversations = async (req: Request, res: Response) => {
     try {
       const ALL_CONVERSTATIONS = await prisma.conversation.findMany();
       res.send(JSON.stringify(ALL_CONVERSTATIONS));
@@ -14,6 +14,21 @@ const getAllConverstations = async (req: Request, res: Response) => {
     }
   };
 
+  const getConversationById = async (req: Request, res: Response) => {
+    try {
+      const CONVERSTATION_ID = req.params.id;
+      const CONVERSTATION_BY_ID = await prisma.conversation.findUnique({
+        where: {
+          id: CONVERSTATION_ID
+        }
+      });
+      res.send(JSON.stringify(CONVERSTATION_BY_ID));
+    } catch (error) {
+      console.error(error);
+      res.send("Error, check console log");
+    } finally {
+      await prisma.$disconnect();
+    }
+  };
 
-  
-export default {getAllConverstations};
+export default {getAllConversations, getConversationById};
--- chatmeleon-server/routes/conversation-route.ts
+++ chatmeleon-server/routes/conversation-route.ts
@@ -2,5 +2,5 @@ import conversationControler from "../controllers/conversation-controller.js"
 import { Router } from 'express';
 
 export const conversationRoute = Router();
-// conversationRoute.get('/conversations/:id', conversationControler.getUserById);
-conversationRoute.get('/conversations', conversationControler.getAllConverstations);
+conversationRoute.get('/conversations/:id', conversationControler.getConversationById);
+conversationRoute.get('/conversations', conversationControler.getAllConversations);
