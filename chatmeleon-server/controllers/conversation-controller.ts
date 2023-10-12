bd56b0e3646f0dfdd12e5605976239c30d996883
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-12 07:49:33 +0700

    CHAT-63: Update conversation-controller with document and new variable name.

--- chatmeleon-server/controllers/conversation-controller.ts
+++ chatmeleon-server/controllers/conversation-controller.ts
@@ -1,34 +1,45 @@
 import prisma from "../libs/prismadb.js"
 import { Request, Response } from "express";
 
-// Define your async route handler function
 const getAllConversations = async (req: Request, res: Response) => {
-    try {
-      const ALL_CONVERSTATIONS = await prisma.conversation.findMany();
-      res.send(JSON.stringify(ALL_CONVERSTATIONS));
-    } catch (error) {
-      console.error(error);
-      res.send("Error, check console log");
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
+  try {
+    // Fetch all conversations from the Prisma database.
+    const allConversations = await prisma.conversation.findMany();
+    
+    // Send the list of conversations as a JSON response.
+    res.send(JSON.stringify(allConversations));
+  } catch (error) {
+    // If there's an error, log it to the console and send an error response.
+    console.error(error);
+    res.send("Error, check console log");
+  } finally {
+    // Disconnect from the Prisma database.
+    await prisma.$disconnect();
+  }
+};
 
-  const getConversationById = async (req: Request, res: Response) => {
-    try {
-      const CONVERSTATION_ID = req.params.id;
-      const CONVERSTATION_BY_ID = await prisma.conversation.findUnique({
-        where: {
-          id: CONVERSTATION_ID
-        }
-      });
-      res.send(JSON.stringify(CONVERSTATION_BY_ID));
-    } catch (error) {
-      console.error(error);
-      res.send("Error, check console log");
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
+const getConversationById = async (req: Request, res: Response) => {
+  try {
+    // Extract the conversation ID from the request parameters.
+    const conversationId = req.params.id;
+    
+    // Query the Prisma database to find a conversation by its ID.
+    const conversationById = await prisma.conversation.findUnique({
+      where: {
+        id: conversationId
+      }
+    });
+    
+    // Send the conversation data as a JSON response.
+    res.send(JSON.stringify(conversationById));
+  } catch (error) {
+    // If there's an error, log it to the console and send an error response.
+    console.error(error);
+    res.send("Error, check console log");
+  } finally {
+    // Disconnect from the Prisma database to release resources.
+    await prisma.$disconnect();
+  }
+};
 
 export default {getAllConversations, getConversationById};
