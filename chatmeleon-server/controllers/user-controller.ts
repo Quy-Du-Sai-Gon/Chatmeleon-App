bf2a3dbc28efdbd8940e14bb1522d9df0fc8edd4
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-12 07:42:32 +0700

    CHAT-62: Updated user-controller.ts with document and change the variable name base on the Guideline.

--- chatmeleon-server/controllers/user-controller.ts
+++ chatmeleon-server/controllers/user-controller.ts
@@ -2,33 +2,48 @@ import prisma from "../libs/prismadb.js"
 import { Request, Response } from "express";
 
 // Define your async route handler function
+
 const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const ALL_USERS = await prisma.user.findMany();
-    res.send(JSON.stringify(ALL_USERS));
+    // Fetch all users from the Prisma database.
+    const allUsers = await prisma.user.findMany();
+    
+    // Send the list of users as a JSON response.
+    res.send(JSON.stringify(allUsers));
   } catch (error) {
+    // If there's an error, log it to the console and send an error response.
     console.error(error);
     res.send("Error, check console log");
   } finally {
+    // Disconnect from the Prisma database.
     await prisma.$disconnect();
   }
 };
 
+
 const getUserById = async (req: Request, res: Response) => {
-    try {
-      const USER_ID = req.params.id;
-      const USER_BY_ID = await prisma.user.findUnique({
-        where: {
-          id: USER_ID,
-        },
-      });
-      res.send(JSON.stringify(USER_BY_ID));
-    } catch (error) {
-      console.error(error);
-      res.send("Error, check console log");
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
+  try {
+    // Extract the user ID from the request parameters.
+    const userId = req.params.id;
+    
+    // Query the Prisma database to find a user by their ID.
+    const userById = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    
+    // Send the user data as a JSON response.
+    res.send(JSON.stringify(userById));
+  } catch (error) {
+    // If there's an error, log it to the console and send an error response.
+    console.error(error);
+    res.send("Error, check console log");
+  } finally {
+    // Disconnect from the Prisma database to release resources.
+    await prisma.$disconnect();
+  }
+};
+
 
 export default {getAllUsers, getUserById};
