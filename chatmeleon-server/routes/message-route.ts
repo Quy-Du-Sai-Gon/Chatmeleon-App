d0752a3a44079aedab1f0a67131ba1a487d4db80
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-16 20:46:09 +0700

    CHAT-64: PUT /messages/:id: Update an entire message by ID

--- chatmeleon-server/routes/message-route.ts
+++ chatmeleon-server/routes/message-route.ts
@@ -5,4 +5,6 @@ export const messageRoute = Router();
 messageRoute.get('/messages/:id', messageController.getMessageById);
 messageRoute.get('/messages', messageController.getAllMessages);
 messageRoute.post('/messages', messageController.createMessage);
-messageRoute.delete('/messages/:id',messageController.deleteMessage);
+messageRoute.put('/messages/:id', messageController.updateMessage);
+messageRoute.patch('/messages/:id', messageController.updateMessageFields);
+messageRoute.delete('/messages/:id', messageController.deleteMessage);
