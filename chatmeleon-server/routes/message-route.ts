0ae5dd6b8c2156b5e922bb84f304bf62d6e9f57c
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-16 20:46:52 +0700

    CHAT-64: PATCH /messages/:id: Update specific field of a message by ID

--- chatmeleon-server/routes/message-route.ts
+++ chatmeleon-server/routes/message-route.ts
@@ -2,6 +2,7 @@ import messageController from "../controllers/message-controller.js";
 import { Router } from 'express';
 
 export const messageRoute = Router();
+
 messageRoute.get('/messages/:id', messageController.getMessageById);
 messageRoute.get('/messages', messageController.getAllMessages);
 messageRoute.post('/messages', messageController.createMessage);
