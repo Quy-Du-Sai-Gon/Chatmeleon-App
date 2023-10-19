2ca2a5b2f109f1543abbf87015ec9f278de39490
Khoa Ngo Dinh Anh <khoango1810@gmail.com>
2023-10-19 20:35:42 +0700

    CHAT-62: Remove test related files.

--- chatmeleon-server/routes/test-route.ts
+++
@@ -1,6 +0,0 @@
-import testRouteHandler from "../controllers/test-controller.js"
-import { Router } from 'express';
-
-export const defaultRoute = Router();
-
-defaultRoute.get('/test', testRouteHandler);
--- chatmeleon-server/src/index.ts
+++ chatmeleon-server/src/index.ts
@@ -1,7 +1,5 @@
-// src/app.ts
 import express from "express";
 import { Request, Response } from "express";
-import { defaultRoute } from '../routes/test-route.js';
 import errorMiddleware from '../middlewares/error-handler.js'
 import { userRoute } from '../routes/user-route.js';
 
@@ -12,13 +10,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Backend server is running.");
 });   
 
-// Testing code
 export const routes = express.Router();
-routes.use(defaultRoute);
 routes.use(userRoute);
 routes.use(errorMiddleware);
 app.use('/', routes);
-// End testing code
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
