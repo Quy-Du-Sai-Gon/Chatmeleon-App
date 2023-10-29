d35e5af45bd94569d7e3a12075a9a6639b7637aa
Khang <xkhang.nguyen2bme@gmail.com>
2023-10-29 23:48:32 +0700

    CHAT-67: Add route for API docs

--- chatmeleon-server/src/index.ts
+++ chatmeleon-server/src/index.ts
@@ -2,11 +2,18 @@
 import express from "express";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
+const swaggerUi = require("swagger-ui-express");
+// Requires 'yamljs' package to load YAML files
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("../documentation/chatmeleon-swagger.yaml");
 
 dotenv.config(); // This will read the .env file and set the environment variables
 
 const app = express();
 const port = process.env.PORT;
+
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World! This is your TypeScript Express server.");
