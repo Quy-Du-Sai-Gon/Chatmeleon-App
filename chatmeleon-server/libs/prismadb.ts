c337f535267343c712839f46fe515b57280965de
xkhang <xkhang.nguyen2bme@gmail.com>
2023-10-30 21:12:13 +0700

    CHAT-58: Remove global variable prisma client

--- chatmeleon-server/libs/prismadb.ts
+++ chatmeleon-server/libs/prismadb.ts
@@ -1,16 +1,8 @@
 // Import the PrismaClient from the "@prisma/client" package
 import { PrismaClient } from "@prisma/client";
 
-// Declare a global variable for the PrismaClient
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
 // Initialize a PrismaClient instance, or use an existing one if available globally
-const client = globalThis.prisma || new PrismaClient();
-
-// If not in production mode, assign the PrismaClient instance to the global variable
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const client = new PrismaClient();
 
 // Export the PrismaClient instance
 export default client;
