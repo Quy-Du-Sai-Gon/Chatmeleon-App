ad712022c933095d5c488b0ec7ae2131e0b93f31
hoangviet10101 <hoangviet10101@gmail.com>
2023-10-31 22:26:59 +0700

    CHAT-60: Update landing page

--- chatmeleon-app/app/(site)/page.tsx
+++ chatmeleon-app/app/(site)/page.tsx
@@ -1,9 +1,62 @@
 export default function Home() {
   return (
-    <div>
-      <p>
-        Chatmeleon app
-      </p>
+    <div className="w-[1920px] h-[1080px] relative bg-gradient-to-b from-white to-rose-600">
+    <div className="w-[1284px] h-[1265px] left-[745px] top-[-633px] absolute bg-rose-600 bg-opacity-70 rounded-full blur-[50px]"></div>
+    <div className="w-[1435px] h-[1435px] left-[648px] top-[-714px] absolute">
+        <div className="w-[721px] h-[724px] left-[394px] top-[1005px] absolute rounded-full border-2 border-black"></div>
+        <div className="w-[835.11px] h-[385.30px] left-[335.59px] top-[952.67px] absolute">
+            <div className="w-[113.71px] h-[113.71px] left-[721.41px] top-[270.33px] absolute">
+                <div className="w-[113.71px] h-[113.71px] left-0 top-0 absolute bg-black rounded-full"></div>
+                <div className="w-[86.57px] h-[86.57px] left-[14.21px] top-[14.21px] absolute">
+                </div>
+            </div>
+            <div className="w-[113.71px] h-[113.71px] left-[360.50px] top-0 absolute">
+                <div className="w-[113.71px] h-[113.71px] left-0 top-0 absolute bg-black rounded-full"></div>
+                <div className="w-[82.70px] h-[82.70px] left-[15.51px] top-[15.51px] absolute">
+                </div>
+            </div>
+            <div className="w-[113.71px] h-[113.71px] left-0 top-[271.59px] absolute">
+                <div className="w-[113.71px] h-[113.71px] left-0 top-0 absolute bg-rose-600 rounded-full shadow"></div>
+            </div>
+        </div>
     </div>
+    <div className="h-[471.68px] left-[1047.72px] top-[453px] absolute">
+        <div className="w-[367.66px] h-[364.81px] left-[172.43px] top-0 absolute">
+            <div className="w-[367.66px] h-[364.81px] left-0 top-0 absolute bg-rose-600 rounded-full shadow-inner"></div>
+        </div>
+        <div className="w-[713.94px] h-[76.95px] left-0 top-[394.73px] absolute text-center text-rose-600 text-5xl font-medium font-['Poppins']">SECURITY</div>
+    </div>
+    <div className="w-[882px] h-[354px] left-[94px] top-[367px] absolute flex-col justify-start items-start inline-flex">
+        <div className="w-[882px] h-[133px] text-rose-600 text-[79px] font-medium font-['Poppins']">Messenger</div>
+        <div className="w-[938px] h-[109px] text-stone-600 text-[57px] font-medium font-['Poppins']">Connect through Interests</div>
+        <div className="w-[819px] h-[175px] text-black text-2xl font-normal font-['Poppins'] leading-[37.92px] tracking-wide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet ipsum, auctor quis sagittis felis risus, a odio. Amet ante nulla sem mauris. Sollicitudin ultrices enim quam.</div>
+    </div>
+    <div className="w-[126px] h-[63px] left-[501.56px] top-[319px] absolute origin-top-left rotate-[19.05deg] text-center text-zinc-800 text-[40px] font-medium font-['Poppins']">New</div>
+    <div className="w-[1749px] h-9 left-[94px] top-[100px] absolute">
+        <div className="w-28 h-[30px] left-[1388px] top-[3px] absolute">
+            <div className="w-[15px] h-[15px] left-0 top-[7px] absolute">
+            </div>
+            <div className="left-[23px] top-0 absolute text-white text-xl font-normal font-['Poppins']">About us</div>
+        </div>
+        <div className="w-[95.89px] h-[30px] left-[1080px] top-[3px] absolute">
+            <div className="left-[20.89px] top-0 absolute text-white text-xl font-normal font-['Poppins']">Sign up</div>
+            <div className="w-[12.88px] h-[15px] left-0 top-[7px] absolute">
+            </div>
+        </div>
+        <div className="w-[76px] h-[30px] left-[792px] top-[3px] absolute">
+            <div className="left-[23px] top-0 absolute text-white text-xl font-normal font-['Poppins']">Login</div>
+            <div className="origin-top-left rotate-[-179.46deg] w-[15px] h-[15px] left-[15px] top-[22.14px] absolute">
+            </div>
+        </div>
+        <div className="w-[30px] h-[30px] left-[1719px] top-[3px] absolute"></div>
+        <div className="w-[198.20px] h-9 left-0 top-0 absolute justify-start items-center gap-3.5 inline-flex">
+            <div className="text-zinc-800 text-2xl font-medium font-['Poppins']">Chatmeleon</div>
+        </div>
+    </div>
+    <div className="w-[458.05px] h-[88px] left-[94px] top-[721px] absolute">
+        <div className="w-[458.05px] h-[88px] left-0 top-0 absolute bg-rose-600 rounded-[48px]"></div>
+        <div className="w-[252.72px] h-[48.51px] left-[102.67px] top-[19.18px] absolute text-center text-white text-[40px] font-bold font-['Poppins'] leading-[63.20px] tracking-wider">Try Now</div>
+    </div>
+</div>
   )
 }
