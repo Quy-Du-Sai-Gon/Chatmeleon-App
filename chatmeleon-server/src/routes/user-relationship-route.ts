8d1d5ac481f8e1fff2f2dbfd3f892591e8c541d9
unknown <thanhbo025@gmail.com>
2023-12-15 00:52:19 +0700

    CHAT-54: Add relationship route

---
+++ chatmeleon-server/src/routes/user-relationship-route.ts
@@ -0,0 +1,9 @@
+import userRelationshipController from "../controllers/user-relationship-controller";
+import { Router } from "express";
+
+export const userRelationshipRoute = Router();
+
+userRelationshipRoute.post(
+  "/friend/send",
+  userRelationshipController.createFriendRequestRelationship
+);
