0c537647c01b1e072d88760eec16cda546f779e5
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-12-20 17:08:40 +0700

    CHAT-72: Add auth to conversation API routes

--- chatmeleon-server/src/routes/conversation-route.ts
+++ chatmeleon-server/src/routes/conversation-route.ts
@@ -1,18 +1,23 @@
 import conversationControler from "../controllers/conversation-controller";
 import { Router } from "express";
+import { requireAuth } from "../middlewares/auth";
 
 export const conversationRoute = Router();
 
 conversationRoute.get(
   "/conversations/:conversationId",
+  requireAuth,
   conversationControler.getConversationById
 );
+
 conversationRoute.get(
-  "/users/:userId/conversations",
-  conversationControler.getConversationsByUserIdWithPagination
+  "/conversations",
+  requireAuth,
+  conversationControler.getConversationsWithPagination
 );
 
 conversationRoute.post(
   "/conversations",
+  requireAuth,
   conversationControler.postConversation
 );
