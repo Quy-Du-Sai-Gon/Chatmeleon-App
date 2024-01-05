3fc41d71e1b87ec026ec71b9f26947240ec90179
unknown <thanhbo025@gmail.com>
2024-01-06 04:18:18 +0700

    CHAT-83: Update and configure prisma error codes

--- chatmeleon-server/src/middlewares/error-handler.ts
+++ chatmeleon-server/src/middlewares/error-handler.ts
@@ -6,42 +6,54 @@ const errorMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error(err); // Log the error for debugging purposes.
+  console.error(err); // Log the error for debugging
 
-  // Check if the error has a 'code' property, which indicates a Prisma error.
   if (err.code) {
     switch (err.code) {
+      // Record not found
+      case "P2001":
+        res.status(404).json({ error: `Not Found: ${err.meta.message}` }); // Respond with 404 Not Found
+        break;
+
+      // Unique constraint violation (e.g., duplicate record)
       case "P2002":
-        // P2002 represents a unique constraint violation in the database, such as trying to insert a duplicate record.
-        // Respond with a 400 Bad Request status and include the specific error message for the client.
-        res.status(400).json({ error: `${err.meta.message}` });
+        res.status(400).json({ error: `Bad Request: ${err.meta.message}` }); // Respond with 400 Bad Request
         break;
-      case "P2014":
-        // P2014 indicates a foreign key constraint violation, typically when trying to reference a non-existent record.
-        // Respond with a 400 Bad Request status and provide the error message to the client.
-        res.status(400).json({ error: `${err.meta.message}` });
-        break;
+
+      // Unique constraint violation (more general)
       case "P2003":
-        // P2003 is another type of unique constraint violation, often used for more general uniqueness checks.
-        // Respond with a 400 Bad Request status and include the error message in the response.
-        res.status(400).json({ error: `${err.meta.message}` });
+        res.status(400).json({ error: `Bad Request: ${err.meta.message}` }); // Respond with 400 Bad Request
         break;
+
+      // Foreign key constraint violation (e.g., referencing non-existent record)
+      case "P2014":
+        res.status(400).json({ error: `Bad Request: ${err.meta.message}` }); // Respond with 400 Bad Request
+        break;
+
+      // Record not found
+      case "P2015":
+        res.status(404).json({ error: `Not Found: ${err.meta.message}` }); // Respond with 404 Not Found
+        break;
+
+      // Inconsistent data column
       case "P2023":
-        // P2023 represents an attempt to perform an operation that violates database constraints.
-        // Respond with a 400 Bad Request status and include the relevant error message in the response.
-        res.status(400).json({ error: `${err.meta.message}` });
+        res.status(400).json({ error: `Bad Request: ${err.meta.message}` }); // Respond with 400 Bad Request
         break;
+
+      // Unique constraint violation (e.g., unauthorized access)
+      case "P2025":
+        res.status(403).json({ error: `Unauthorized` }); // Respond with 403 Forbidden
+        break;
+
+      // Unexpected Prisma error
       default:
-        // If the Prisma error code is not recognized, it could be an unexpected Prisma error.
-        // Respond with a 500 Internal Server Error and include a general error message.
-        res.status(500).json({ error: `Something went wrong: ${err.message}` });
+        res.status(500).json({ error: `Something went wrong: ${err.message}` }); // Internal server error
         break;
     }
   } else if (err instanceof Error) {
-    res.status(500).json({ error: `Network error: ${err.message}` });
+    res.status(500).json({ error: `Network error: ${err.message}` }); // Network error
   } else {
-    // Handle any other unknown errors
-    res.status(500).json({ error: "Something went wrong" });
+    res.status(500).json({ error: "Something went wrong" }); // Unknown error
   }
 };
 
