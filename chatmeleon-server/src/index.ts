b4ae1c498f416d66d4bc71eaaf94cf44de91537b
MapleThanh <thanhbo025@gmail.com>
2024-01-14 21:49:42 +0700

    CHAT-83: Delete prisma error middleware

--- chatmeleon-server/src/index.ts
+++ chatmeleon-server/src/index.ts
@@ -13,8 +13,6 @@ const swaggerDocument = YAML.load("../documentation/chatmeleon-swagger.yaml");
 
 dotenv.config();
 
-
-import errorMiddleware from "./middlewares/error-handler";
 import { messageRoute } from "./routes/message-route";
 import { conversationRoute } from "./routes/conversation-route";
 import { userRoute } from "./routes/user-route";
@@ -35,8 +33,6 @@ app.use(express.json());
 app.use(messageRoute);
 app.use(conversationRoute);
 app.use(userRoute);
-
-app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
