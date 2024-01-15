638a5f4fca3ce255ddc2105986790f2e18c4c477
MapleThanh <thanhbo025@gmail.com>
2024-01-16 01:46:31 +0700

    Revert "CHAT-83: Delete prisma error middleware"
    This reverts commit b4ae1c498f416d66d4bc71eaaf94cf44de91537b.

--- chatmeleon-server/src/index.ts
+++ chatmeleon-server/src/index.ts
@@ -13,6 +13,8 @@ const swaggerDocument = YAML.load("../documentation/chatmeleon-swagger.yaml");
 
 dotenv.config();
 
+
+import errorMiddleware from "./middlewares/error-handler";
 import { messageRoute } from "./routes/message-route";
 import { conversationRoute } from "./routes/conversation-route";
 import { userRoute } from "./routes/user-route";
@@ -33,6 +35,8 @@ app.use(express.json());
 app.use(messageRoute);
 app.use(conversationRoute);
 app.use(userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
