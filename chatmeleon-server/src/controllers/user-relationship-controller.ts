309cbd806b688a8cb5af935e7225dfe856674426
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2024-02-03 15:55:35 +0700

    CHAT-37: Reimplement controllers

--- chatmeleon-server/src/controllers/user-relationship-controller.ts
+++ chatmeleon-server/src/controllers/user-relationship-controller.ts
@@ -2,120 +2,217 @@ import prisma from "../libs/prismadb";
 import { Request, Response } from "express";
 
 const createFriendRequestRelationship = async (req: Request, res: Response) => {
-  const { relatingUserId, relatedUserId } = req.body;
-  const relationship = await prisma.user_Relationship.createMany({
-    data: [
-      {
-        type: "SENDING_FRIEND_REQUEST",
-        relatingUserId: relatingUserId,
-        relatedUserId: relatedUserId,
-      },
-      {
-        type: "PENDING_FRIEND_REQUEST",
-        relatingUserId: relatedUserId,
-        relatedUserId: relatingUserId,
-      },
-    ],
-  });
-
-  res.json(relationship);
+  const { relatedUserId } = req.body;
+  const { userId: relatingUserId } = req.auth!;
+  // Utilize a Prisma transaction to ensure atomicity and data consistency
+  try {
+    await prisma.$transaction(async (tx) => {
+      // Check if the users are the same
+      if (relatingUserId === relatedUserId) {
+        throw new Error("Invalid: Cannot create relationship with oneself");
+      }
+      // Check if the relevant records already exist
+      const existingRelationship = await tx.user_Relationship.findMany({
+        where: {
+          OR: [
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "FRIEND",
+            },
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "SENDING_FRIEND_REQUEST",
+            },
+          ],
+        },
+      });
+      if (existingRelationship && existingRelationship.length == 1) {
+        if (existingRelationship[0].type == "FRIEND") {
+          // Records already exist, handle accordingly
+          throw new Error(
+            'Conflicted: Relationship of "FRIEND" type already existed'
+          ); // Return an error code
+        } else if (existingRelationship[0].type == "SENDING_FRIEND_REQUEST") {
+          // Records already exist, handle accordingly
+          throw new Error(
+            "Conflicted: Relationship of this type already existed"
+          ); // Return an error code
+        }
+      }
+      // Create new records
+      await tx.user_Relationship.createMany({
+        data: [
+          {
+            relatingUserId: relatingUserId,
+            relatedUserId: relatedUserId,
+            type: "SENDING_FRIEND_REQUEST",
+          },
+          {
+            relatingUserId: relatedUserId,
+            relatedUserId: relatingUserId,
+            type: "PENDING_FRIEND_REQUEST",
+          },
+        ],
+      });
+    });
+  } catch (error) {
+    // Throw the error to trigger the transaction rollback
+    // Log the error and send a 403 Unauthorized response
+    console.error("Transaction failed -", error);
+    return res
+      .status(403)
+      .type("text/plain")
+      .send("Unauthorized: User is unauthorized");
+  }
+  // All operations successful, send a success response
+  res.status(200).type("text/plain").send("OK");
 };
 
 const createFriendRelationship = async (req: Request, res: Response) => {
-  const { relatingUserId, relatedUserId } = req.body;
-
-  const existingRelationship = await prisma.user_Relationship.findMany({
-    where: {
-      OR: [
-        {
-          relatingUserId: relatingUserId,
-          relatedUserId: relatedUserId,
-          type: "PENDING_FRIEND_REQUEST",
+  const { relatedUserId } = req.body;
+  const { userId: relatingUserId } = req.auth!;
+  // Utilize a Prisma transaction to ensure atomicity and data consistency
+  try {
+    await prisma.$transaction(async (tx) => {
+      // Check if the users are the same
+      if (relatingUserId === relatedUserId) {
+        throw new Error("Invalid: Cannot create relationship with oneself");
+      }
+      // Check if the records already exist
+      const existingRelationship = await tx.user_Relationship.findMany({
+        where: {
+          OR: [
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "FRIEND",
+            },
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "SENDING_FRIEND_REQUEST",
+            },
+          ],
         },
-        {
-          relatingUserId: relatedUserId,
-          relatedUserId: relatingUserId,
-          type: "SENDING_FRIEND_REQUEST",
+      });
+      if (existingRelationship && existingRelationship.length == 1) {
+        if (existingRelationship[0].type == "FRIEND") {
+          // Records already exist, handle accordingly
+          throw new Error(
+            "Conflicted: Relationship of this type already existed"
+          ); // Return an error code
+        } else if (existingRelationship[0].type == "SENDING_FRIEND_REQUEST") {
+          // User not authorized, handle accordingly
+          throw new Error(
+            "Unauthorized: User not authorized to modify the relationship"
+          ); // Return an error code
+        }
+      }
+      // Create new records
+      await tx.user_Relationship.updateMany({
+        where: {
+          OR: [
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "PENDING_FRIEND_REQUEST",
+            },
+            {
+              relatingUserId: relatedUserId,
+              relatedUserId: relatingUserId,
+              type: "SENDING_FRIEND_REQUEST",
+            },
+          ],
         },
-      ],
-    },
-  });
-
-  if (existingRelationship && existingRelationship.length === 2) {
-    const updateRelationship = await prisma.user_Relationship.updateMany({
-      where: {
-        OR: [
-          {
-            relatingUserId: relatingUserId,
-            relatedUserId: relatedUserId,
-            type: "PENDING_FRIEND_REQUEST",
-          },
-          {
-            relatingUserId: relatedUserId,
-            relatedUserId: relatingUserId,
-            type: "SENDING_FRIEND_REQUEST",
-          },
-        ],
-      },
-      data: {
-        type: "FRIEND",
-      },
+        data: {
+          type: "FRIEND",
+        },
+      });
     });
-
-    res.status(200).json({
-      message: "Friend relationship created successfully",
-      data: updateRelationship,
-    });
-  } else {
-    res.status(404).json({ message: "No pending friend request found" });
+  } catch (error) {
+    // Throw the error to trigger the transaction rollback
+    // Log the error and send a 403 Unauthorized response
+    console.error("Transaction failed -", error);
+    return res
+      .status(403)
+      .type("text/plain")
+      .send("Unauthorized: User is unauthorized");
   }
+  // All operations successful, send a success response
+  res.status(200).type("text/plain").send("OK");
 };
 
 const deleteFriendRequestRelationship = async (req: Request, res: Response) => {
-  const { relatingUserId, relatedUserId } = req.body;
-
-  const existingRelationship = await prisma.user_Relationship.findMany({
-    where: {
-      OR: [
-        {
-          relatingUserId: relatingUserId,
-          relatedUserId: relatedUserId,
-          type: "PENDING_FRIEND_REQUEST",
+  const { relatedUserId } = req.body;
+  const { userId: relatingUserId } = req.auth!;
+  // Utilize a Prisma transaction to ensure atomicity and data consistency
+  try {
+    await prisma.$transaction(async (tx) => {
+      // Check if the users are the same
+      if (relatingUserId === relatedUserId) {
+        throw new Error("Invalid: Cannot delete relationship with oneself");
+      }
+      // Check if the records already exist
+      const existingRelationship = await tx.user_Relationship.findMany({
+        where: {
+          OR: [
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "FRIEND",
+            },
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "SENDING_FRIEND_REQUEST",
+            },
+          ],
         },
-        {
-          relatingUserId: relatedUserId,
-          relatedUserId: relatingUserId,
-          type: "SENDING_FRIEND_REQUEST",
+      });
+      if (existingRelationship && existingRelationship.length == 1) {
+        if (existingRelationship[0].type == "FRIEND") {
+          // Records already exist, handle accordingly
+          throw new Error(
+            'Conflicted: Relationship of "FRIEND" type already existed'
+          ); // Return an error code
+        } else if (existingRelationship[0].type == "SENDING_FRIEND_REQUEST") {
+          // User not authorized, handle accordingly
+          throw new Error(
+            "Unauthorized: User not authorized to modify the relationship"
+          ); // Return an error code
+        }
+      }
+      // Create new records
+      await tx.user_Relationship.deleteMany({
+        where: {
+          OR: [
+            {
+              relatingUserId: relatingUserId,
+              relatedUserId: relatedUserId,
+              type: "PENDING_FRIEND_REQUEST",
+            },
+            {
+              relatingUserId: relatedUserId,
+              relatedUserId: relatingUserId,
+              type: "SENDING_FRIEND_REQUEST",
+            },
+          ],
         },
-      ],
-    },
-  });
-
-  if (existingRelationship && existingRelationship.length === 2) {
-    const updateRelationship = await prisma.user_Relationship.deleteMany({
-      where: {
-        OR: [
-          {
-            relatingUserId: relatingUserId,
-            relatedUserId: relatedUserId,
-            type: "PENDING_FRIEND_REQUEST",
-          },
-          {
-            relatingUserId: relatedUserId,
-            relatedUserId: relatingUserId,
-            type: "SENDING_FRIEND_REQUEST",
-          },
-        ],
-      },
+      });
     });
-
-    res.status(200).json({
-      message: "Friend request relationship deleted successfully",
-      data: updateRelationship,
-    });
-  } else {
-    res.status(404).json({ message: "No pending friend request found" });
+  } catch (error) {
+    // Throw the error to trigger the transaction rollback
+    // Log the error and send a 403 Unauthorized response
+    console.error("Transaction failed -", error);
+    return res
+      .status(403)
+      .type("text/plain")
+      .send("Unauthorized: User is unauthorized");
   }
+  // All operations successful, send a success response
+  res.status(200).type("text/plain").send("OK");
 };
 
 export default {
