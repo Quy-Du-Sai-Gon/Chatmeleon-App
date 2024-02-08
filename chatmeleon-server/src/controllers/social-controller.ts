573a836a673004f9a8ce17242ad683bdd317c257
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2024-02-09 06:28:32 +0700

    CHAT-37: Create social controller and route

---
+++ chatmeleon-server/src/controllers/social-controller.ts
@@ -0,0 +1,12 @@
+import prisma from "../libs/prismadb";
+import { Request, Response } from "express";
+
+// TO-DO for Nam
+const getUsersByStringWithPagination = async (
+  req: Request,
+  res: Response
+) => {};
+
+export default {
+  getUsersByStringWithPagination,
+};
