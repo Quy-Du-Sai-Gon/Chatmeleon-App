e8253df48b7b705ea5784058abaf29b6707c76f3
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-03-10 22:15:45 +0700

    CHAT-96: Add comments to validation util types

--- chatmeleon-server/src/validation/utils.ts
+++ chatmeleon-server/src/validation/utils.ts
@@ -5,6 +5,9 @@ import {
   PickTypeMayBe,
 } from "../types/utils";
 
+/**
+ * Construct a type with the properties of T, making nullable properties optional, excluding null values, and omitting properties that can only be null or undefined.
+ */
 export type Pruned<T> = OmitType<
   {
     [K in keyof PickTypeMayBe<T, Nullish>]?: Exclude<T[K], Nullish>;
@@ -14,6 +17,9 @@ export type Pruned<T> = OmitType<
   undefined
 >;
 
+/**
+ * Returns a new object with properties from the passed in object, excluding null or undefined values (pruned).
+ */
 export function pruneObject<T extends object>(obj: T): Pruned<T> {
   return Object.fromEntries(
     Object.entries(obj).filter(([_, v]) => v != null)
